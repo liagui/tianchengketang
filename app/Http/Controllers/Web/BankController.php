7689c1e8cb9872a6fb6cb08ced810dbfc58d92a7
Suzhenwen <2663930382@qq.com>
2020-10-22 17:51:03 +0800

    题库

--- app/Http/Controllers/Web/BankController.php
+++ app/Http/Controllers/Web/BankController.php
@@ -1281,7 +1281,7 @@ class BankController extends Controller {
 
         //判断学员的答案是否和正确答案相同
          if($exam_info['type'] == 5){
-           print_r($myanswer);die;
+             $myanswer = implode(',',$myanswer);
          }
         if(stringSort(trim($exam_info['answer'])) != stringSort(trim($myanswer))) {
             $is_right = 2;
