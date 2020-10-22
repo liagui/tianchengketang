cac90eea3f815133f31441a429be890740db6c4f
Suzhenwen <2663930382@qq.com>
2020-10-22 17:54:11 +0800

    题库

--- app/Http/Controllers/Web/BankController.php
+++ app/Http/Controllers/Web/BankController.php
@@ -1281,6 +1281,7 @@ class BankController extends Controller {
 
         //判断学员的答案是否和正确答案相同
          if($exam_info['type'] == 5){
+             print_r($myanswer);die;
              $myanswer = implode(',',$myanswer);
              if($exam_info['answer'] != trim($myanswer)) {
                  $is_right = 2;
