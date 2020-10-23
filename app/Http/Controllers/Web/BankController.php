baef66db87ac825f30368d3893f711737208562f
Suzhenwen <2663930382@qq.com>
2020-10-23 19:22:01 +0800

    题库

--- app/Http/Controllers/Web/BankController.php
+++ app/Http/Controllers/Web/BankController.php
@@ -1281,7 +1281,7 @@ class BankController extends Controller {
         $exam_info = Exam::where("id" , $exam_id)->first();
         //判断学员的答案是否和正确答案相同
         if($exam_info['type'] == 5){
-            if(strstr($myanswer,'，') == false){
+            if(strstr($myanswer,',') == false){
                 if($exam_info['answer'] == $myanswer){
                     $is_right = 11;
                 }else{
