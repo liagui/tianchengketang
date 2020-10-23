8b9320ccb5519632ace5e3dd0836290306404c3b
sxh <shixinghua_0207@163.com>
2020-10-23 18:46:45 +0800

    添加列表排序

--- app/Models/CouresSubject.php
+++ app/Models/CouresSubject.php
@@ -27,6 +27,7 @@ class CouresSubject extends Model {
        }
        $list =self::select('id','subject_name','description','is_open')
            ->where($where)
+		   ->orderBy(DB::Raw('case when sort =0 then 999999 else sort end'),'asc')
            ->get()->toArray();
        foreach ($list as $k=>&$v){
            $sun = self::select('id','subject_name','is_open')
