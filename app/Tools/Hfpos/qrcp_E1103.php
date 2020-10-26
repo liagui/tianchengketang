d6b28fb013999aa372ed0ec594834eab57d8a278
liagui <315142971@qq.com>
2020-10-26 20:44:05 +0800

    测试汇付

--- app/Tools/Hfpos/qrcp_E1103.php
+++ app/Tools/Hfpos/qrcp_E1103.php
@@ -2,12 +2,12 @@
 namespace App\Tools\Hfpos;
 class qrcp_E1103{
     public function Hfpos($data){
-    echo "123";
+
     include "commons/function.php";
     $apiurl = 'https://nspos.cloudpnr.com/qrcp/E1103';
         $jsonData   = utf8_encode(json_encode($data));
         $checkValue = getSign($jsonData);
-         echo $checkValue;
+        
         $request = array(
             'jsonData'   => $jsonData,
             'checkValue' => $checkValue
@@ -15,7 +15,7 @@ class qrcp_E1103{
         $result  = http_post($apiurl, $request);
 
         $resultArr = json_decode($result, true);
-        print_r($resultArr);die;
+        return $resultArr;
 //            if ($resultArr['respCode'] == 000000) {
 //                $verify = verifySign($resultArr['jsonData'], $resultArr['checkValue']);
 //            }
