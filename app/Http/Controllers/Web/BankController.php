e064b5a916c2915359c05467ebc03c2b5d57096a
Suzhenwen <2663930382@qq.com>
2020-10-28 10:47:52 +0800

    显示第一步

--- app/Http/Controllers/Web/BankController.php
+++ app/Http/Controllers/Web/BankController.php
@@ -349,6 +349,7 @@ class BankController extends Controller {
                     $examtype = Exam::where(['bank_id'=>$bank_id,'subject_id'=>$subject_id,'chapter_id'=>$chapter_id,'is_del'=>0,'is_publish'=>1,'type'=>7])->get()->toArray();
                     foreach ($examtype as $ks => $vs){
                         $type7 = Exam::where(['is_del'=>0,'is_publish'=>1,'parent_id'=>$vs['id']])->count();
+                        echo $type7.'-----';
                         $exam_type_array[6]['count'] = $type7 + $exam_type_array[6]['count'];
                     }
                 }
