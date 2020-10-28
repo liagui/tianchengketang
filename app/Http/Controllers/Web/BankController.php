576d8434725d6f9b3bcead4b4a1341103a3bc868
Suzhenwen <2663930382@qq.com>
2020-10-28 16:33:21 +0800

    显示第一步

--- app/Http/Controllers/Web/BankController.php
+++ app/Http/Controllers/Web/BankController.php
@@ -2661,14 +2661,14 @@ class BankController extends Controller {
                 return response()->json(['code' => 200 , 'msg' => '交卷成功' , 'data' => ['answer_time' => $info['answer_time'] , 'answer_score' => 0]]);
             }
         } else if($type == 3){  //模拟真题
-            echo "屠吉利=====";
+            echo "1111111111111111111=====";
             //新数组赋值
             $sum_score = [];
             $exam_array = [];
 
             //根据学员做题试卷id获取试卷得id
             $papersId = StudentPapers::where('id' , $papers_id)->value('papers_id');
-            echo $papersId.'///////';
+            echo $papersId.'========';
             //通过试卷id获取试卷详情
             $papers_info = Papers::where("id" , $papersId)->first();
             print_r($papers_info);
@@ -2711,7 +2711,7 @@ class BankController extends Controller {
                         }
                     }
                     $sum_scores = count($sum_score) > 0 ? array_sum($sum_score) : 0;
-                    echo $sum_scores.'屠益达';
+                    echo $sum_scores.'===========';
 
                     //更新试卷的信息
                     $id = StudentPapers::where(['student_id' => self::$accept_data['user_info']['user_id'] , 'bank_id' => $bank_id , 'subject_id' => $subject_id , 'id' => $papers_id , 'type' => 3])->update(['answer_time' => $answer_time , 'answer_score' => $sum_scores , 'is_over' => 1 , 'update_at' => date('Y-m-d H:i:s')]);
@@ -2818,7 +2818,7 @@ class BankController extends Controller {
                         return response()->json(['code' => 203 , 'msg' => '交卷失败']);
                     }
                 } else {
-                    echo "屠美玲";
+                    echo "333===================";
                     //查询还未做完的题列表
                     $noexam_list = StudentDoTitle::where("student_id" , self::$accept_data['user_info']['user_id'])->where("bank_id" , $bank_id)->where("subject_id" , $subject_id)->where('papers_id' , $papers_id)->where('type' , 3)->where('is_right' , 0)->get()->toArray();
                     if($noexam_list && !empty($noexam_list)){
