9c0fa051d54fcd294e95b052728221abd7925780
Suzhenwen <2663930382@qq.com>
2020-10-28 20:26:02 +0800

    显示第一步

--- app/Http/Controllers/Web/BankController.php
+++ app/Http/Controllers/Web/BankController.php
@@ -360,10 +360,9 @@ class BankController extends Controller {
                     $exam_type_array[$k]['count'] = $questcount + $exam_type_array[$k]['count'];
                 }else{
                     $examtype = Exam::where(['bank_id'=>$bank_id,'subject_id'=>$subject_id,'chapter_id'=>$chapter_id,'joint_id'=>$joint_id,'is_del'=>0,'is_publish'=>1,'type'=>7])->get()->toArray();
-                    print_r($examtype);die;
                     if(!empty($examtype)){
                         foreach ($examtype as $ks => $vs){
-                            $type7 = Exam::where(['bank_id'=>$bank_id,'subject_id'=>$subject_id,'chapter_id'=>$chapter_id,'joint_id'=>$joint_id,'is_del'=>0,'is_publish'=>1,'parent_id'=>$vs['id']])->count();
+                            $type7 = Exam::where(['is_del'=>0,'is_publish'=>1,'parent_id'=>$vs['id']])->count();
                             $exam_type_array[6]['count'] = $type7 + $exam_type_array[6]['count'];
                         }
                     }
