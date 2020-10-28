84869ba1a71f20bd5e11f11d4da1fe55cf89e54e
sxh <shixinghua_0207@163.com>
2020-10-28 11:17:10 +0800

    diaoshijiekou

--- app/Exports/BankListExport.php
+++ app/Exports/BankListExport.php
@@ -18,7 +18,7 @@ class BankListExport implements FromCollection, WithHeadings {
 		var_dump($data);
         //获取学员做题信息
         $studentList = StudentPapers::getStudentBankList($data);
-		var_dump($studentList);
+		var_dump($studentList['data']);
         //return $studentList['data'];
     }
 
