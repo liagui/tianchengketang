76560869ece66dbde7e7b3bdb2c70ba47aa41cc6
sxh <shixinghua_0207@163.com>
2020-10-29 17:30:06 +0800

    sxh

--- app/Models/StudentPapers.php
+++ app/Models/StudentPapers.php
@@ -29,8 +29,8 @@ class StudentPapers extends Model {
         }
         //没有分页
         $studentList = self::getStudentBankInfo($data);
-		var_dump($studentList);die();
-        //return ['code' => 200 , 'msg' => '获取做题记录列表成功','data'=>$studentList];
+		
+        return ['code' => 200 , 'msg' => '获取做题记录列表成功','data'=>$studentList];
     }
 
 
