c8a1a0a0e9497fd98b4a2899a77507e5557639af
sxh <shixinghua_0207@163.com>
2020-10-30 14:10:12 +0800

    前端提交评论功能

--- routes/web.php
+++ routes/web.php
@@ -216,6 +216,9 @@ $router->group(['prefix' => 'web' , 'namespace' => 'Web'], function () use ($rou
         $router->post('courseToUser','CourseController@courseToUser');//用户与课程关系
         $router->post('recordeurl','CourseController@recordeurl');//课程录播url
         $router->post('liveurl','CourseController@liveurl');//课程直播url
+        $router->post('comment','CourseController@comment');//评论课程
+
+
     });
     //站内支付
     $router->group(['prefix' => 'order', 'middleware'=> 'user'], function () use ($router) {
