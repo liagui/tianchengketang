6ab1724f40413f3b40ef247823ec9be2c490d71d
Suzhenwen <2663930382@qq.com>
2020-10-30 19:50:53 +0800

    题库

--- app/Http/Controllers/Web/BankController.php
+++ app/Http/Controllers/Web/BankController.php
@@ -876,7 +876,7 @@ class BankController extends Controller {
                 $papers_id = $student_papers_info['id'];
 
                 //查询还未做完的题列表
-                $exam_list = StudentDoTitle::where("student_id" , self::$accept_data['user_info']['user_id'])->where("papers_id" , $papers_id)->where('type' , 2)->get();
+                $exam_list = StudentDoTitle::where("student_id" , self::$accept_data['user_info']['user_id'])->where("papers_id" , $papers_id)->where('type' , 2)->get()->toArray();
                 print_r($exam_list);die;
                 foreach($exam_list as $k=>$v) {
                     if ($v['quert_type'] == 7) {
