e09af3a2ebabca5740ce6f6f26a3d56937046c56
sxh <shixinghua_0207@163.com>
2020-11-03 11:34:10 +0800

    修改web端课程评论接口-sxh

--- app/Http/Controllers/Web/CourseController.php
+++ app/Http/Controllers/Web/CourseController.php
@@ -963,18 +963,11 @@ class CourseController extends Controller {
             $pagesize = isset($this->data['pagesize']) && $this->data['pagesize'] > 0 ? $this->data['pagesize'] : 20;
             $page     = isset($this->data['page']) && $this->data['page'] > 0 ? $this->data['page'] : 1;
             $offset   = ($page - 1) * $pagesize;
-			$user_id = $this->userid;
-            //获取列表
+			/获取列表
             $list = Comment::leftJoin('ld_student','ld_student.id','=','ld_comment.uid')
                 ->leftJoin('ld_school','ld_school.id','=','ld_comment.school_id')
                 ->where(['ld_comment.school_id' => $this->school['id'], 'ld_comment.course_id'=>$this->data['course_id'], 'ld_comment.nature'=>$this->data['nature'], 'ld_comment.status'=>1])
-                ->where(function($query) use ($user_id){
-                    //用户是否为空
-                    if(isset($user_id) && $user_id > 0){
-                        $query->where('ld_comment.uid' , '=' , $user_id);
-                    }
-                })
-				->select('ld_comment.id','ld_comment.create_at','ld_comment.content','ld_comment.course_name','ld_comment.teacher_name','ld_comment.score','ld_comment.anonymity','ld_student.real_name','ld_student.nickname','ld_student.head_icon as user_icon','ld_school.name as school_name')
+                ->select('ld_comment.id','ld_comment.create_at','ld_comment.content','ld_comment.course_name','ld_comment.teacher_name','ld_comment.score','ld_comment.anonymity','ld_student.real_name','ld_student.nickname','ld_student.head_icon as user_icon','ld_school.name as school_name')
                 ->orderByDesc('ld_comment.create_at')->offset($offset)->limit($pagesize)
                 ->get()->toArray();
             foreach($list as $k=>$v){
