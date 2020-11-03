03d24de41cb6bd65e41e4849855388c5bad3c09d
sxh <shixinghua_0207@163.com>
2020-11-03 19:47:56 +0800

    我的消息

---
+++ app/Models/MyMessage.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+
+use App\Tools\CurrentAdmin;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Redis;
+class MyMessage extends Model {
+    //指定别的表名
+    public $table = 'ld_my_message';
+    //时间戳设置
+    public $timestamps = false;
+
+
+
+
+}
