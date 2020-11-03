f12e4e8c7a59c8d70c6352959523c0eff6dc3a7e
sxh <shixinghua_0207@163.com>
2020-11-03 11:52:13 +0800

    我的评论路由-sxh

--- routes/web.php
+++ routes/web.php
@@ -208,6 +208,7 @@ $router->group(['prefix' => 'web' , 'namespace' => 'Web'], function () use ($rou
         $router->post('myCourse','UserController@myCourse');//我的课程
         $router->post('doLoginOut','UserController@doLoginOut');//Web端退出登录接口
 		$router->post('myMessage','UserController@myMessage');//我的消息
+		$router->post('myCommen','UserController@myCommen');//评论列表
     });
     //课程（szw）
     $router->group(['prefix' => 'course', 'middleware'=> 'user'], function () use ($router) {
