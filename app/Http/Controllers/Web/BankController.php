80cd0809730b41710e2d686099ba17e7c4f40603
Suzhenwen <2663930382@qq.com>
2020-11-03 20:35:23 +0800

    试题展示

--- app/Http/Controllers/Web/BankController.php
+++ app/Http/Controllers/Web/BankController.php
@@ -741,7 +741,6 @@ class BankController extends Controller {
             $exam_array = [];
             //判断是否做完了随机生成的快速做题数量
             $rand_exam_count = StudentDoTitle::where("student_id" , self::$accept_data['user_info']['user_id'])->where("bank_id" , $bank_id)->where("subject_id" , $subject_id)->where('is_right' , 0)->where('type' , 2)->count();
-            echo $rand_exam_count;die;
             if($rand_exam_count <= 0){
                 //快速做题随机生成20条数据
                 $exam_list = Exam::where(['bank_id' => $bank_id,'subject_id' => $subject_id,'is_del' => 0,'is_publish' =>1])->orderByRaw("RAND()")->limit(20)->get();
@@ -869,6 +868,7 @@ class BankController extends Controller {
                 $papers_id = $student_papers_info['id'];
                 //查询还未做完的题列表
                 $exam_list = StudentDoTitle::where("student_id" , self::$accept_data['user_info']['user_id'])->where("papers_id" , $papers_id)->where('type' , 2)->get()->toArray();
+                print_r($exam_list);die;
                 foreach($exam_list as $k=>$v) {
                     if ($v['quert_type'] == 7) {
                         //先获取材料子题
