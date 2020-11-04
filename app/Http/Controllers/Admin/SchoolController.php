a6fc9c0cdb0caebdf63d055bfb05cbb91095ea79
zhaolaoxian <191824710@qq.com>
2020-11-04 16:02:45 +0800

    完善网校状态

--- app/Http/Controllers/Admin/SchoolController.php
+++ app/Http/Controllers/Admin/SchoolController.php
@@ -208,7 +208,7 @@ class SchoolController extends Controller {
         if($validator->fails()) {
             return response()->json(json_decode($validator->errors()->first(),1));
         }
-        if(in_array($data['status'],[1,2,3,0]))
+        if(in_array($data['status'],[1,2,3,4,5,0]))
         {
             return response()->json(['code'=>204,'msg'=>'无效的状态']);
         }
@@ -240,7 +240,6 @@ class SchoolController extends Controller {
                     $yl_pay_state = 1;
                     break;
                 case 2://禁用前台
-                    $is_forbid = 1;
                     $wx_pay_state = -1;
                     $zfb_pay_state = -1;
                     $hj_wx_pay_state = -1;
@@ -249,11 +248,16 @@ class SchoolController extends Controller {
                     break;
                 case 3://禁用后台
                     $is_forbid = 0;
+                    break;
+                case 4://启用前台
                     $wx_pay_state = 1;
                     $zfb_pay_state = 1;
                     $hj_wx_pay_state = 1;
                     $hj_zfb_pay_state = 1;
                     $yl_pay_state = 1;
+                    break;
+                case 5://启用后台
+                    $is_forbid = 0;
                     break;
             }
 
