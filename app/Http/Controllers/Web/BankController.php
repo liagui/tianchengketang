eef07dd1c68a3115c5a5258aaefc1b6a7c801b54
Suzhenwen <2663930382@qq.com>
2020-11-05 11:45:37 +0800

    试卷分数

--- app/Http/Controllers/Web/BankController.php
+++ app/Http/Controllers/Web/BankController.php
@@ -1256,7 +1256,7 @@ class BankController extends Controller {
         //通过题库和科目id查询试卷的列表
         $exam_array = Papers::where("bank_id" , $bank_id)->where("subject_id" , $subject_id)->where("is_del" , 0)->where("is_publish" , 1)->get()->toArray();
         if(!$exam_array || empty($exam_array)){
-            return response()->json(['code' => 200 , 'msg' => '暂无对应的试卷']);
+            return response()->json(['code' => 200 , 'msg' => '暂无对应的试卷','data'=>[]]);
         }
 
         //数组赋值
