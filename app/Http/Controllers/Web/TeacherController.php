5858aa45ee038f40dfabe726148d4f0a28e17fa5
ysh <ysh198554@126.com>
2020-11-10 09:13:48 +0800

    fix

--- app/Http/Controllers/Web/TeacherController.php
+++ app/Http/Controllers/Web/TeacherController.php
@@ -183,74 +183,56 @@ class TeacherController extends Controller {
 	}
     //列表
     public function getListByIndexSet(){
-        $type = !isset($this->data['type']) || $this->data['type']<=0 ?0:$this->data['type'];
-
-        $teacherArr = Teacher::where(['school_id'=>$this->school['id'],'is_del'=>0,'is_forbid'=>0,'type'=>2])->select('id','head_icon','real_name','describe','number','is_recommend','teacher_icon')->orderBy('number','desc')->get()->toArray(); //自增讲师
-
-        $natureTeacherArr = CourseRefTeacher::leftJoin('ld_lecturer_educationa','ld_lecturer_educationa.id','=','ld_course_ref_teacher.teacher_id')
-            ->where(['ld_course_ref_teacher.to_school_id'=>$this->school['id'],'ld_course_ref_teacher.is_del'=>0,'ld_lecturer_educationa.type'=>2])
-            ->select('ld_lecturer_educationa.id','ld_lecturer_educationa.head_icon','ld_lecturer_educationa.real_name','ld_lecturer_educationa.describe','ld_lecturer_educationa.number','ld_lecturer_educationa.is_recommend','ld_lecturer_educationa.teacher_icon')->get()->toArray();//授权讲师
-
-        if(!empty($natureTeacherArr)){
-
-            foreach($natureTeacherArr as $key=>&$v){
+        $limit = 8;
+        $courseRefTeacher = CourseRefTeacher::leftJoin('ld_lecturer_educationa','ld_lecturer_educationa.id','=','ld_course_ref_teacher.teacher_id')
+            ->where(['to_school_id'=>$this->school['id'],'type'=>2])
+            ->select('ld_lecturer_educationa.id','ld_lecturer_educationa.head_icon','ld_lecturer_educationa.real_name','ld_lecturer_educationa.describe','ld_lecturer_educationa.number','ld_lecturer_educationa.teacher_icon')
+            ->limit($limit)->get()->toArray(); //授权讲师
+        $courseRefTeacher = array_unique($courseRefTeacher, SORT_REGULAR);
+        $count = count($courseRefTeacher);
+        if($count >0){ //授权讲师信息
+            foreach($courseRefTeacher as $key=>&$teacher){
                 $natureCourseArr =  CourseSchool::leftJoin('ld_course_teacher','ld_course_teacher.course_id','=','ld_course_school.course_id')
                     ->leftJoin('ld_lecturer_educationa','ld_lecturer_educationa.id','=','ld_course_teacher.teacher_id')
-                    ->where(['ld_course_school.is_del'=>0,'ld_course_school.to_school_id'=>$this->school['id'],'ld_course_school.status'=>1,'ld_lecturer_educationa.id'=>$v['id']])
-                    ->select('ld_course_school.id as course_id ','ld_course_school.cover','ld_course_school.title','ld_course_school.pricing','ld_course_school.buy_num','ld_lecturer_educationa.id')
+                    ->where(['ld_course_school.is_del'=>0,'ld_course_school.to_school_id'=>$this->school['id'],'ld_course_school.status'=>1,'ld_lecturer_educationa.id'=>$teacher['id']])
+                    ->select('ld_course_school.cover','ld_course_school.title','ld_course_school.pricing','ld_course_school.buy_num','ld_lecturer_educationa.id as teacher_id','ld_course_school.id as course_id')
                     ->get()->toArray();
                 $courseIds = array_column($natureCourseArr, 'course_id');
-                $v['number'] = count($natureCourseArr);//开课数量
+                $teacher['number'] = count($natureCourseArr);//开课数量
                 $sumNatureCourseArr = array_sum(array_column($natureCourseArr,'buy_num'));//虚拟购买量
-                $realityBuyum= Order::whereIn('class_id',$courseIds)->where(['school_id'=>$this->school['id'],'nature'=>1,'status'=>2])->whereIn('pay_status',[3,4])->count();//实际购买量 （授权课程订单class_id 对应的是ld_course_school 的id ）
-                $v['student_number'] = $sumNatureCourseArr+$realityBuyum;
-                $v['grade'] =  '5.0';
-                $v['star_num'] = 5;
-                $v['is_nature'] = 1;
+                $realityBuyum= Order::whereIn('class_id',$courseIds)->where(['school_id'=>$this->school['id'],'nature'=>1,'status'=>2])->whereIn('pay_status',[3,4])->count();//实际购买量（授权课程订单class_id 对应的是ld_course_school 的id ）
+                $teacher['student_number'] = $sumNatureCourseArr+$realityBuyum;
+                $teacher['is_nature'] = 1;
+                $teacher['star_num']= 5;
             }
         }
+        if($count<$limit){
+            //自增讲师信息
+            $teacherData = Teacher::where(['school_id'=>$this->school['id'],'is_del'=>0,'type'=>2])->orderBy('number','desc')->select('id','head_icon','real_name','describe','number','teacher_icon')->limit($limit-$count)->get()->toArray();
+            $teacherDataCount = count($teacherData);
+            if($teacherDataCount >0){
+                foreach($teacherData as $key=>&$vv){
+                    $couresArr  = Coures::leftJoin('ld_course_teacher','ld_course_teacher.course_id','=','ld_course.id')
+                        ->leftJoin('ld_lecturer_educationa','ld_lecturer_educationa.id','=','ld_course_teacher.teacher_id')
+                        ->where(['ld_course.is_del'=>0,'ld_course.school_id'=>$this->school['id'],'ld_course.status'=>1,'ld_lecturer_educationa.id'=>$vv['id']])
+                        ->select('ld_course.cover','ld_course.title','ld_course.pricing','ld_course.buy_num','ld_lecturer_educationa.id as teacher_id','ld_course.id as course_id')
+                        ->get()->toArray();
 
-        if(!empty($teacherArr)){
-            foreach($teacherArr as $key=>&$vv){
-                $couresArr  = Coures::leftJoin('ld_course_teacher','ld_course_teacher.course_id','=','ld_course.id')
-                    ->leftJoin('ld_lecturer_educationa','ld_lecturer_educationa.id','=','ld_course_teacher.teacher_id')
-                    ->where(['ld_course.is_del'=>0,'ld_course.school_id'=>$this->school['id'],'ld_course.status'=>1,'ld_lecturer_educationa.id'=>$vv['id']])
-                    ->select('ld_course.cover','ld_course.title','ld_course.pricing','ld_course.buy_num','ld_lecturer_educationa.id','ld_course.id as course_id')
-                    ->get()->toArray();
-
-                $courseIds = array_column($couresArr, 'course_id');
-                $vv['number'] = count($couresArr);//开课数量
-                $sumNatureCourseArr = array_sum(array_column($couresArr,'buy_num'));//虚拟购买量
-                $realityBuyum = Order::whereIn('class_id',$courseIds)->where(['school_id'=>$this->school['id'],'nature'=>0,'status'=>2])->whereIn('pay_status',[3,4])->count();//实际购买量
-                $vv['student_number'] = $sumNatureCourseArr+$realityBuyum;
-                $vv['grade'] =  '5.0';
-                $vv['star_num'] = 5;
-                $vv['is_nature'] = 0;
+                    $courseIds = array_column($couresArr, 'course_id');
+                    $vv['number'] = count($couresArr);//开课数量
+                    $sumNatureCourseArr = array_sum(array_column($couresArr,'buy_num'));//虚拟购买量
+                    $realityBuyum = Order::whereIn('class_id',$courseIds)->where(['school_id'=>$this->school['id'],'nature'=>0,'status'=>2])->whereIn('pay_status',[3,4])->count();//实际购买量
+                    $vv['student_number'] = $sumNatureCourseArr+$realityBuyum;
+                    $vv['grade'] =  '5.0';
+                    $vv['star_num'] = 5;
+                    $vv['is_nature'] = 0;
+                }
             }
-        }
-
-        if(!empty($natureTeacherArr) || !empty($teacherArr)){
-            $teacherData = array_merge($natureTeacherArr,$teacherArr);
-            if( $type==1 ){
-                $sort = array_column($teacherData, 'student_number');
-                array_multisort($sort, SORT_DESC, $teacherData);
-            }
-            $teacherData = array_unique($teacherData, SORT_REGULAR);
+            $recomendTeacherArr=array_merge($courseRefTeacher,$teacherData);
         }else{
-            $teacherData=[];
+            $recomendTeacherArr = $courseRefTeacher;
         }
-        $pagesize = isset($this->data['pagesize']) && $this->data['pagesize'] > 0 ? $this->data['pagesize'] : 20;
-        $page     = isset($this->data['page']) && $this->data['page'] > 0 ? $this->data['page'] : 1;
-
-        $start=($page-1)*$pagesize;
-        $limit_s=$start+$pagesize;
-        $info=[];
-        for($i=$start;$i<$limit_s;$i++){
-            if(!empty($teacherData[$i])){
-                array_push($info,$teacherData[$i]);
-            }
-        }
-        return response()->json(['code'=>200,'msg'=>'Succes','data'=>$info,'total'=>count($teacherData)]);
+        return response()->json(['code'=>200,'msg'=>'Success','data'=>$recomendTeacherArr]);
 
     }
 }
