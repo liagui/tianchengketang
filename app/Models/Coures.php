de123385aed406878145e780d3f8805b5f9b160d
sxh <shixinghua_0207@163.com>
2020-11-11 18:14:22 +0800

    添加课程bug-sxh

--- app/Models/Coures.php
+++ app/Models/Coures.php
@@ -609,6 +609,7 @@ class Coures extends Model {
 
     //添加
     public static function courseAdd($data){
+		$school_id = isset(AdminLog::getAdminInfo()->admin_user->school_id)?AdminLog::getAdminInfo()->admin_user->school_id:0;
         if(empty($data) || !isset($data)){
             return ['code' => 201 , 'msg' => '传参数组为空'];
         }
@@ -641,9 +642,10 @@ class Coures extends Model {
         if(!isset($data['introduce']) || empty($data['introduce'])){
             return ['code' => 201 , 'msg' => '课程简介不能为空'];
         }
-		$title = self::where(['title'=>$data['title'],'is_del'=>0])->first();
+		//课程标题是否重复
+        $title = self::where(['title'=>$data['title'],'is_del'=>0,'school_id'=>$school_id])->first();
         if($title){
-            return ['code' => 201 , 'msg' => '课程标题已存在'];
+            return ['code' => 201 , 'msg' => '课程名称已存在'];
         }
         $user_id = isset(AdminLog::getAdminInfo()->admin_user->id)?AdminLog::getAdminInfo()->admin_user->id:0;
         //入课程表
@@ -835,12 +837,6 @@ class Coures extends Model {
 		$school_id = isset(AdminLog::getAdminInfo()->admin_user->school_id)?AdminLog::getAdminInfo()->admin_user->school_id:0;
         if(empty($data) || !isset($data)){
             return ['code' => 201 , 'msg' => '传参数组为空'];
-        }
-		
-		//课程标题是否重复
-        $title = self::where(['title'=>$data['title'],'is_del'=>0,'school_id'=>$school_id])->first();
-        if($title){
-            return ['code' => 201 , 'msg' => '课程名称已存在'];
         }
 		
         DB::beginTransaction();
