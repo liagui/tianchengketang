bf67113c77d794d34ce379024ff94e2a588c63b5
sxh <shixinghua_0207@163.com>
2020-11-14 12:01:29 +0800

    学科小类bug

--- app/Models/CouresSubject.php
+++ app/Models/CouresSubject.php
@@ -41,7 +41,7 @@ class CouresSubject extends Model {
     //添加
     public static function subjectAdd($user_id,$school_id,$data){
 		//判断学科的唯一性
-        $name = empty($data['subject_name']) ? '大类' : '小类';
+        $name = $data['subject_name']==0 ? '大类' : '小类';
         $find = self::where(['admin_id'=>$user_id,'school_id'=>$school_id,'subject_name'=>$data['subject_name'],'is_del'=>0,'parent_id'=>$data['parent_id']])->first();
 		if($find){
                  return ['code' => 203 , 'msg' => '学科'.$name.'已存在'];
