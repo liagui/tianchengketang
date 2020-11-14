e8dec076769ea1301a38e4f2457c10f4eaa1a3a5
sxh <shixinghua_0207@163.com>
2020-11-14 17:32:22 +0800

    修改排课bug

--- app/Models/Live.php
+++ app/Models/Live.php
@@ -686,7 +686,7 @@ class Live extends Model {
                     if($method<=0){
                         unset($list[$k]);
                     }
-                    $res = Subject::where("is_del",0)->where("id",$live['child_id'])->select("subject_name")->first()['subject_name'];
+                    $res = Subject::where("is_del",0)->where("id",$live['child_id'])->select("subject_name")->first();
                     if(!empty($res)){
                         $live['subject_child_name'] = $res;
                     }else{
