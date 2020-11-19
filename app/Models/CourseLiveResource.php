96ef54a4e676e1f89f6b95ab5bdd8268fb27d6e3
Suzhenwen <2663930382@qq.com>
2020-11-19 10:00:05 +0800

    admin  kecheng

--- app/Models/CourseLiveResource.php
+++ app/Models/CourseLiveResource.php
@@ -64,11 +64,11 @@ class CourseLiveResource extends Model {
             ->where('ld_course_livecast_resource.is_forbid','<',2)
             ->orderByDesc('ld_course_livecast_resource.id')->get()->toArray();
         foreach ($livecast as $k=>&$v){
-//            $arr = LiveClass::where(['resource_id'=>$v,'is_del'=>0,'is_forbid'=>0])->first();
-//            if(empty($arr)){
-//                unset($livecast[$k]);
-//                continue;
-//            }
+            $arr = LiveClass::where(['resource_id'=>$v['id'],'is_del'=>0,'is_forbid'=>0])->first();
+            if(empty($arr)){
+                unset($livecast[$k]);
+                continue;
+            }
             $ones = CouresSubject::where('id',$v['parent_id'])->first();
             if(!empty($ones)){
                 $v['parent_name'] = $ones['subject_name'];
