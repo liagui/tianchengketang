6a3bab4f8d921e8bfca71d6f9bb88aceb9a770f3
Suzhenwen <2663930382@qq.com>
2020-11-19 17:31:38 +0800

    web tiku

--- app/Http/Controllers/Web/BankController.php
+++ app/Http/Controllers/Web/BankController.php
@@ -248,7 +248,7 @@ class BankController extends Controller {
                 $chapters_list = $chapters_list->toArray();
                 foreach ($chapters_list as $k => $v) {
                     //根据章id获取节列表
-                    $joint_list = Chapters::select('id as joint_id', 'name as joint_name')->where("bank_id", $bank_id)->where("subject_id", $subject_id)->where('parent_id', $v['id'])->where("type", 1)->where("is_del", 0)->get()->toArray();
+                    $joint_list = Chapters::select('id as joint_id', 'name as joint_name')->where("bank_id", $bank_id)->where("subject_id", $subject_id)->where('parent_id', $v['id'])->where("type", 1)->where("is_del", 0)->get();
                     if ($joint_list && !empty($joint_list)) {
                         $joint_list = $joint_list->toArray();
                         foreach ($joint_list as $k1 => $v1) {
