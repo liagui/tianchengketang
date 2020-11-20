a8cf8f86bfdbfd4ea24d4b6c8e013eafa0620b6c
shixinghua <shixinghua_0207@163.com>
2020-11-20 17:00:26 +0800

    修改回答次数

--- app/Http/Controllers/Web/AnswersController.php
+++ app/Http/Controllers/Web/AnswersController.php
@@ -141,6 +141,13 @@ class AnswersController extends Controller {
         if(empty($data['user_type']) || !isset($data['user_type'])){
             return ['code' => 201 , 'msg' => '回复的用户类型为空'];
         }
+		//一分钟内不得频繁提交内容
+            $time = date ( "Y-m-d H:i:s" , strtotime ( "-1 minute" ));
+            $date = date ( "Y-m-d H:i:s" , time());
+            $list = AnswersReply::where(['answers_id'=>$data['id'],'user_id'=>$this->userid])->whereBetween('create_at',[$time,$date])->select('id','create_at')->orderByDesc('create_at')->count();
+            if($list>2){
+                return response()->json(['code' => 202, 'msg' => '操作太频繁,1分钟以后再来吧']);
+            }
         DB::beginTransaction();
         try{
             //插入数据
