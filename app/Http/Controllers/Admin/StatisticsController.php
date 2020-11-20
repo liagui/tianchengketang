59f6700fa70f61537c2f644eadbd6ddb870e1166
Suzhenwen <2663930382@qq.com>
2020-11-20 19:50:10 +0800

    学员判断

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -114,7 +114,7 @@ class StatisticsController extends Controller {
                    $query->where('ld_student.reg_source',$data['source']);
                }
                //用户类型
-               if(!empty($data['enroll_status'])&&$data['enroll_status'] != ''){
+               if(isset($data['enroll_status'])){
                    $query->where('ld_student.enroll_status',$data['enroll_status']);
                }
                //用户姓名
@@ -162,7 +162,7 @@ class StatisticsController extends Controller {
                        $query->where('ld_student.reg_source',$data['source']);
                    }
                    //用户类型
-                   if(!empty($data['enroll_status'])&&$data['enroll_status'] != ''){
+                   if(isset($data['enroll_status'])){
                        $query->where('ld_student.enroll_status',$data['enroll_status']);
                    }
                    //用户姓名
