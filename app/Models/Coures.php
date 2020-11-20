adafa37bcd59d040fa33a3ddaf9b9b56e8a92f3b
shixinghua <shixinghua_0207@163.com>
2020-11-21 02:52:16 +0800

    bb

--- app/Models/Coures.php
+++ app/Models/Coures.php
@@ -1487,8 +1487,8 @@ class Coures extends Model {
         $user_id = isset(AdminLog::getAdminInfo()->admin_user->cur_admin_id)?AdminLog::getAdminInfo()->admin_user->cur_admin_id:0;
 		$school_id = isset(AdminLog::getAdminInfo()->admin_user->school_id) ? AdminLog::getAdminInfo()->admin_user->school_id : 0;
         //插入课程数据
-        $title = self::where(['title'=>$data['title'],'is_del'=>0,'nature'=>1])->first();
-		return ['code' => 201 , 'msg' => $data['title']];
+        $title = self::where(['title'=>$data['title'],'is_del'=>0])->first();
+
         if($title){
             return ['code' => 201 , 'msg' => '课程标题已存在'];
         }
