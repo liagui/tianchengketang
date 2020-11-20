94e622766201c03e331f900a67d02dad2ca3756d
shixinghua <shixinghua_0207@163.com>
2020-11-21 00:58:50 +0800

    bug

--- app/Models/Coures.php
+++ app/Models/Coures.php
@@ -122,9 +122,9 @@ class Coures extends Model {
         }
         $list=[];
         if($count > 0){
-			echo 1;
+			
             if(!isset($data['nature']) || empty($data['nature'])){
-				echo 2;
+				
                 //全部
                 $list1 = self::where(['is_del'=>0])->where(function($query) use ($data,$school_id) {
                     //判断总校 查询所有或一个分校
@@ -176,6 +176,10 @@ class Coures extends Model {
                         }
                         $v['method'] = $method;
                     }
+					$buy_nember = Order::whereIn('pay_status',[3,4])->where('nature',1)->where(['school_id'=>$school_id,'class_id'=>$v['id'],'status'=>2,'oa_status'=>1])->count();
+                    $sum_nember = CourseStocks::where(['school_pid'=>1,'school_id'=>$school_id,'course_id'=>$v['course_id'],'is_del'=>0])->sum('add_number');
+                    $list1[$k]['surplus'] = $sum_nember-$buy_nember <=0 ? 0 : $sum_nember-$buy_nember; //剩余库存量
+					$list1[$k]['sum_nember'] = $sum_nember; //剩余库存量
                 }
                 $list2 = CourseSchool::where(['is_del'=>0])->where(function($query) use ($data,$school_id) {
                     //判断总校 查询所有或一个分校
@@ -228,10 +232,14 @@ class Coures extends Model {
                         }
                         $vs['method'] = $method;
                     }
+					$buy_nember = Order::whereIn('pay_status',[3,4])->where('nature',1)->where(['school_id'=>$school_id,'class_id'=>$v['id'],'status'=>2,'oa_status'=>1])->count();
+                    $sum_nember = CourseStocks::where(['school_pid'=>1,'school_id'=>$school_id,'course_id'=>$v['course_id'],'is_del'=>0])->sum('add_number');
+                    $list2[$k]['surplus'] = $sum_nember-$buy_nember <=0 ? 0 : $sum_nember-$buy_nember; //剩余库存量
+					$list2[$k]['sum_nember'] = $sum_nember; //剩余库存量
                 }
                 $list =array_slice(array_merge($list1,$list2),($page - 1) * $pagesize, $pagesize);
             }else if($data['nature']-1 == 1){
-				echo 3;
+				
                 //授权
                 $list = CourseSchool::where(['is_del'=>0])->where(function($query) use ($data,$school_id) {
                     //判断总校 查询所有或一个分校
@@ -291,7 +299,7 @@ class Coures extends Model {
 						
                     }
             }else{
-				echo 4;
+				
                 //自增
                 $list = self::where(['is_del'=>0])->where(function($query) use ($data,$school_id) {
                     //判断总校 查询所有或一个分校
