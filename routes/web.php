12f3a9ef6adf5c1f1e4b5f36b7cb74fc2a5e996b
shixinghua <shixinghua_0207@163.com>
2020-11-23 15:43:15 +0800

    学习记录-直播

--- routes/web.php
+++ routes/web.php
@@ -521,7 +521,7 @@ $router->group(['prefix' => 'admin' , 'namespace' => 'Admin' , 'middleware'=> ['
 		$router->post('getStudentBankList', 'StudentController@getStudentBankList');     //学员做题记录
         $router->post('getStudentBankSearchInfo', 'StudentController@getStudentBankSearchInfo');     //筛选学员做题记录条件
 		$router->post('getStudentBankDetails', 'StudentController@getStudentBankDetails');     //学员做题记录详情
-		//$router->post('getStudentStudyList', 'StudentController@getStudentStudyList');     //学员学习记录
+		$router->post('getStudentStudyList', 'StudentController@getStudentStudyList');     //学员学习记录
     });
 
 
