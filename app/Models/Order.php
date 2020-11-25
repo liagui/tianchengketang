644b57ad58ca459f5e0c570fc67eb25a5ae9fdf1
shixinghua <shixinghua_0207@163.com>
2020-11-25 15:04:01 +0800

    测试

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1148,6 +1148,7 @@ class Order extends Model {
 
                 }
             }
+			var_dump($res);die();
             return ['code' => 200 , 'msg' => '获取学习记录成功-直播课' , 'data'=>$res];
         }
        
