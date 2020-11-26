8204faed6d8573cf919e5e13669ccbb685661916
shixinghua <shixinghua_0207@163.com>
2020-11-26 23:47:33 +0800

    测试

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1269,7 +1269,7 @@ class Order extends Model {
                 $res[$k]['child_name'] =  CouresSubject::where(['id'=>$course_open_live_childs['child_id']])->select('subject_name')->first()['subject_name'];
             }
             //课程
-            $class_list = CourseLiveClassChild::rightJoin('ld_course_class_number','ld_course_class_number.id','=','ld_course_live_childs.class_id')
+            /*$class_list = CourseLiveClassChild::rightJoin('ld_course_class_number','ld_course_class_number.id','=','ld_course_live_childs.class_id')
                 ->rightJoin('ld_course_shift_no','ld_course_shift_no.id','=','ld_course_class_number.shift_no_id')
                 ->where(['course_id'=>$v['course_id']])
                 ->select('ld_course_live_childs.course_name as kecheng','ld_course_class_number.name as keci','ld_course_shift_no.name as banhao','ld_course_shift_no.resource_id')
@@ -1291,7 +1291,7 @@ class Order extends Model {
                 $res[$k]['unit'] = $class_list['keci'];
                 $res[$k]['class'] = $class_list['banhao'];
                 $res[$k]['child_name'] =  $class_name;
-            }
+            }*/
         }
         return ['code' => 200 , 'msg' => '获取直播到课率成功' , 'data'=>$res];
     }
