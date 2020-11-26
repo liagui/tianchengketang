c17032de63a4336425fb3a0619ed3279a80793c4
shixinghua <shixinghua_0207@163.com>
2020-11-26 10:21:26 +0800

    测试

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1087,7 +1087,7 @@ class Order extends Model {
 
             }
         }
-		return ['code' => 200 , 'msg' => '获取学习记录成功-录播课' , 'data'=>$list];
+		
 		if($data['type'] ==1){
             //直播课次
             foreach ($list as $k => $v){
@@ -1114,8 +1114,9 @@ class Order extends Model {
                         $coures_school_list[$ks]['max_class_time'] = date("Y-m-d  H:i:s",time());
                     }
                 }
+				//var_dump($coures_school_list);die();
                 //自增课程
-                if($v['nature'] == 0) {
+                /*if($v['nature'] == 0) {
                     $list[$k]['coures'] = Coures::leftJoin('ld_course_live_resource','ld_course_live_resource.course_id','=','ld_course.id')
                         ->leftJoin('ld_course_shift_no','ld_course_shift_no.id','=','ld_course_live_resource.shift_id')
                         ->leftJoin('ld_course_class_number','ld_course_class_number.shift_no_id','=','ld_course_shift_no.id')
@@ -1135,7 +1136,7 @@ class Order extends Model {
                         $coures_list[$ks]['is_finish'] = '未完成';
                         $coures_list[$ks]['max_class_time'] = date("Y-m-d  H:i:s",time());
                     }
-                }
+                }*/
             }
 			/*$res = [];
             if(!empty($coures_list) && !empty($coures_school_list)){
@@ -1160,7 +1161,7 @@ class Order extends Model {
 				return ['code' => 200 , 'msg' => '获取学习记录成功-直播课' , 'data'=>$res];
             }
 			$res = (object)$res; //duixiang*/
-			var_dump($coures_school_list);die();
+			//var_dump($coures_school_list);die();
             return ['code' => 200 , 'msg' => '获取学习记录成功-直播课' , 'data'=>$coures_school_list];
         }
 		if($data['type'] ==2){
