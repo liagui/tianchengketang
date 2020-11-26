2a6e5aaef95814630770898eb7aa74a4fe735a94
马延平 <ma895@163.com>
2020-11-26 19:55:45 +0800

    更新 连接并发数 流量的统计信息

--- app/Console/Commands/CCConnectionsCron.php
+++ app/Console/Commands/CCConnectionsCron.php
@@ -99,8 +99,10 @@ class CCConnectionsCron extends Command
             $log =  "school:[$school_name][$school_id]:当日已经使用的并发数:[$key_now_num]=>[$now_num]".PHP_EOL;
             $this->consoleAndLog($log);
 
+            //var_dump($now_num);
+
             // 写入日志写入到并发日志中 表示当前已经使用的日志
-            $connection_log->addLog($school_id, $key_now_num, SchoolConnectionsLog::CONN_CHANGE_LOG, $yesterday);
+            $connection_log->addUsedLog($school_id, $now_num, SchoolConnectionsLog::CONN_CHANGE_LOG, $yesterday);
 
             //todo 当并发数统计中发现最大的超过的并发数 是否禁止网校登录？？
 
@@ -108,11 +110,9 @@ class CCConnectionsCron extends Command
             $month_num_used = $school_conn_dis->getDistributionByDate($school_id, date("Y-m-d"));
             Redis::set($key,$month_num_used);
             Redis::set($key_now_num,0);
-            $log =  "school:[$school_name][$school_id]:更新本月可用的并发数:[$key]=>[$month_num_used]".PHP_EOL;
+            $log =  "school:[$school_name][$school_id]::[$key]=>[$month_num_used]".PHP_EOL;
             $this->consoleAndLog($log);
-
-
-
+            
         }
 
         Log::info('CC直播并发数统计完毕！');
