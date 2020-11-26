20f18efa5364be8c33019f00196f9eedfcbe9cc1
shixinghua <shixinghua_0207@163.com>
2020-11-26 10:50:18 +0800

    测试

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1165,7 +1165,7 @@ class Order extends Model {
             }
 			$res = (object)$res; //duixiang*/
 			//var_dump($coures_school_list);die();
-            return ['code' => 200 , 'msg' => '获取学习记录成功-直播课' , 'data'=>$coures];
+            return ['code' => 200 , 'msg' => '获取学习记录成功-直播课' , 'data'=>$coures_school_list];
         }
 		if($data['type'] ==2){
 			echo 2;
