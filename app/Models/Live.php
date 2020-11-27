d32877c773ab4cd8bc0791b885d0ab1881dc03e7
shixinghua <shixinghua_0207@163.com>
2020-11-27 08:14:10 +0800

    测试

--- app/Models/Live.php
+++ app/Models/Live.php
@@ -713,12 +713,12 @@ class Live extends Model {
                     if($method<=0){
                         unset($list[$k]);
                     }
-                    $res = Subject::where("is_del",0)->where("id",$live['child_id'])->select("subject_name")->first();
+                    /*$res = Subject::where("is_del",0)->where("id",$live['child_id'])->select("subject_name")->first();
                     if(!empty($res)){
                         $list[$k]['subject_child_name'] = $res['subject_name'];
                     }else{
                         $list[$k]['subject_child_name'] = "";
-                    }
+                    }*/
                     $gl = CourseLiveResource::select("course_id")->where("is_del",0)->where("course_id",$live['id'])->where("resource_id",$data['resource_id'])->first();
                     if(empty($gl)){
                         $list[$k]['is_relevance'] = 0;
