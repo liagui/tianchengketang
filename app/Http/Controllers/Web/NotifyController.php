72ac6cdcbaf720ac3671f88ef98be4db2f964c23
Suzhenwen <2663930382@qq.com>
2020-11-28 18:01:45 +0800

    admin payhuiju

--- app/Http/Controllers/Web/NotifyController.php
+++ app/Http/Controllers/Web/NotifyController.php
@@ -126,10 +126,9 @@ class NotifyController extends Controller {
         }
     }
     public function ylwebnotify(){
-//        $xml = file_get_contents('php://input');
-//        $arr = $this->xmlstr_to_array($xml);
-//        file_put_contents('ylwebnotify.txt', '时间:' . date('Y-m-d H:i:s') . print_r($arr, true), FILE_APPEND);
-        $arr= $_POST;
+        $xml = file_get_contents('php://input');
+        $arr = $this->xmlstr_to_array($xml);
+        file_put_contents('ylwebnotify.txt', '时间:' . date('Y-m-d H:i:s') . print_r($arr, true), FILE_APPEND);
         if($arr['status'] == 0){
             $orders = Order::where(['order_number'=>$arr['out_trade_no']])->first();
             if ($orders['status'] > 0) {
