aa858b70ee82a9c685e3482a33e33a41db60cd7a
liagui <315142971@qq.com>
2020-12-07 21:01:39 +0800

    优化服务充值

--- app/Models/ServiceController.php
+++ app/Models/ServiceController.php
@@ -298,7 +298,7 @@ class ServiceController extends Controller {
         }
         $validator = Validator::make($post, [
             'num' => 'required|min:1|integer',
-            'money' => 'required|min:1|numeric',
+            'money' => 'required|numeric',
             'start_time' => 'required|date',
             'end_time' => 'required|date',
         ],Service::message());
@@ -359,7 +359,7 @@ class ServiceController extends Controller {
         $validator = Validator::make($post, [
             //'num' => 'required|min:1|integer',
             'month' => 'required|min:1|integer',
-            'money' => 'required|min:1|numeric'
+            'money' => 'required|numeric'
         ],ServiceRecord::message());
         if ($validator->fails()) {
             return response()->json(json_decode($validator->errors()->first(),true));
@@ -412,7 +412,7 @@ class ServiceController extends Controller {
         }
         $validator = Validator::make($post, [
             'num' => 'required|min:1|integer',
-            'money' => 'required'
+            'money' => 'required|numeric'
         ],ServiceRecord::message());
         if ($validator->fails()) {
             return response()->json(json_decode($validator->errors()->first(),true));
@@ -485,7 +485,7 @@ class ServiceController extends Controller {
 
         $validator = Validator::make($post, [
             'num' => 'required|min:1|integer',
-            'money' => 'required|min:1|numeric'
+            'money' => 'required|numeric'
         ],ServiceRecord::message());
         if ($validator->fails()) {
             return response()->json(json_decode($validator->errors()->first(),true));
