13403af441589c8481a85287a8d5f4b74368b6c6
Suzhenwen <2663930382@qq.com>
2020-12-21 14:45:13 +0800

    web 微信支付

--- app/Tools/Wx/WxpayFactory.php
+++ app/Tools/Wx/WxpayFactory.php
@@ -67,7 +67,6 @@ class WxpayFactory{
         $response = $this->postXmlCurl($xml, $url);
         //将微信返回的结果xml转成数组
         $res = $this->xmlstr_to_array($response);
-        file_put_contents('wxpayyyyyyyyyyy.txt', '时间:'.date('Y-m-d H:i:s').print_r($res,true),FILE_APPEND);
         if($res['return_code'] != 'Success'){
             $arr = array('code'=>204,'data'=>$res['return_msg']);
         }else {
@@ -97,6 +96,7 @@ class WxpayFactory{
         $response = $this->postXmlCurl($xml, $url);
         //将微信返回的结果xml转成数组
         $res = $this->xmlstr_to_array($response);
+        file_put_contents('wxpayyyyyyyyyyy.txt', '时间:'.date('Y-m-d H:i:s').print_r($res,true),FILE_APPEND);
         if($res['return_code'] != 'SUCCESS'){
             $arr = array('code'=>204,'data'=>$res['return_msg']);
         }else{
