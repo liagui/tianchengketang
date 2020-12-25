538470ce7f56717babd6430d91f4b491c1cca8e8
Suzhenwen <2663930382@qq.com>
2020-12-25 14:37:08 +0800

    admin 学科小类展示问题

--- app/Models/CouresSubject.php
+++ app/Models/CouresSubject.php
@@ -217,7 +217,7 @@ class CouresSubject extends Model {
                     if(!empty($childcourse)){
                         foreach ($childcourse as $childk => $childv){
                             $twsss = self::select('id','parent_id','admin_id','school_id','subject_name as name','subject_cover as cover','subject_cover as cover','description','is_open','is_del','create_at')->where(['id'=>$childv['child_id'],'is_del'=>0,'is_open'=>0])->first();
-                            $twos['childs'][] = $twsss;
+                            $twos['childs'] = $twsss;
                         }
                     }
                     $two[] =$twos;
