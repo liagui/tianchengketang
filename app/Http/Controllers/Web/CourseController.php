7c695273a2c45411d88dd16dcb7aa4834a729507
Suzhenwen <2663930382@qq.com>
2021-01-07 20:47:12 +0800

    web课程显示

--- app/Http/Controllers/Web/CourseController.php
+++ app/Http/Controllers/Web/CourseController.php
@@ -421,22 +421,30 @@ class CourseController extends Controller {
                 }
             }
             $ordercount = Order::where(['status' => 2, 'oa_status' => 1, 'school_id' => $this->school['id'], 'class_id' => $this->data['id'], 'nature' => 1])->whereIn('pay_status',[3,4])->count();
-            if($ordercount >= $stocknum){
-                $data['is_pay'] = 2;
-            }else{
-                //是否已购买
-                if($this->userid != 0){
-                    $order = Order::where(['student_id' => $this->userid, 'class_id' =>$this->data['id'], 'status' => 2,'nature'=>1])->whereIn('pay_status',[3,4])->orderByDesc('id')->first();
-                    //看订单里面的到期时间 进行判断
-                    if (date('Y-m-d H:i:s') >= $order['validity_time']) {
-                        //课程到期  只能观看
+            if($this->userid != 0){
+                $order = Order::where(['student_id' => $this->userid, 'class_id' =>$this->data['id'], 'status' => 2,'nature'=>1])->whereIn('pay_status',[3,4])->orderByDesc('id')->first();
+                //看订单里面的到期时间 进行判断
+                if (date('Y-m-d H:i:s') >= $order['validity_time']) {
+                    if($ordercount >= $stocknum){
+                        //库存不足
+                        $data['is_pay'] = 2;
+                    }else {
+                        //课程到期 需要购买
                         $data['is_pay'] = 0;
-                    } else {
-                        $data['is_pay'] = 1;
                     }
+                } else {
+                    //已经购买
+                    $data['is_pay'] = 1;
+                }
+            }else{
+                if($ordercount >= $stocknum){
+                    //库存不足
+                    $data['is_pay'] = 2;
                 }else{
+                    //需要购买
                     $data['is_pay'] = 0;
                 }
+
             }
             //判断用户是否收藏
             if($this->userid != 0){
