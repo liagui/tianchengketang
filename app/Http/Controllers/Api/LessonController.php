7a61d866094900ee6c4fd8017e83555dda458eea
张泽坤 <31497246+zzk3279372968@users.noreply.github.com>
2021-01-08 10:50:47 +0800

    app 资料更新

--- app/Http/Controllers/Api/LessonController.php
+++ app/Http/Controllers/Api/LessonController.php
@@ -415,8 +415,9 @@ class LessonController extends Controller {
                     if(empty($newhello)){
                         $newhello = [];
                     }
-                    $lesson['url_zhibo'] = $newhello;
-                    $lesson['url_dianbo'] = $ziyuan;
+                    $lesson['url'] = array_merge($newhello,$ziyuan);
+                    $lesson['url_zhibo'] = $ziyuan;
+                    $lesson['url_dianbo'] = $newhello;
                     //授权课程
                     CourseSchool::where('course_id', $request->input('id'))->update(['watch_num' => DB::raw('watch_num + 1'),'update_at'=>date('Y-m-d H:i:s')]);
                 }else{
@@ -495,8 +496,10 @@ class LessonController extends Controller {
                     if(empty($newhello)){
                         $newhello = [];
                     }
-                    $lesson['url_zhibo'] = $newhello;
-                    $lesson['url_dianbo'] = $ziyuan;
+                    $lesson['url'] = array_merge($newhello,$ziyuan);
+                    $lesson['url_zhibo'] = $ziyuan;
+                    $lesson['url_dianbo'] = $newhello;
+
                 }
         }else{
             $lesson = Lesson::select("*","pricing as price","sale_price as favorable_price","expiry as ttl","introduce as introduction","describe as description")->where("school_id",1)->find($request->input('id'));
@@ -560,8 +563,9 @@ class LessonController extends Controller {
             if(empty($newhello)){
                 $newhello = [];
             }
-            $lesson['url_zhibo'] = $newhello;
-            $lesson['url_dianbo'] = $ziyuan;
+            $lesson['url'] = array_merge($newhello,$ziyuan);
+            $lesson['url_zhibo'] = $ziyuan;
+            $lesson['url_dianbo'] = $newhello;
             $lesson['is_collection'] = 0;
             $lesson['is_buy'] = 0;
             //学习人数   基数+订单数
@@ -571,7 +575,6 @@ class LessonController extends Controller {
         $ordernum = Order::where(['class_id' => $lesson['id'], 'status' => 2, 'oa_status' => 1])->count();
         $lesson['buy_num'] = $lesson['buy_num'] + $ordernum;
         //自增课程
-        unset($lesson['url']);
         Lesson::where('id', $request->input('id'))->update(['watch_num' => DB::raw('watch_num + 1'),'update_at'=>date('Y-m-d H:i:s')]);
         return $this->response($lesson);
     }
