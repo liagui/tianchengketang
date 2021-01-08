6cbbb6a910b2b2e56869d134031b31de4073e3ec
张泽坤 <31497246+zzk3279372968@users.noreply.github.com>
2021-01-08 09:43:33 +0800

    更新

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -116,9 +116,9 @@ $app->register(\Maatwebsite\Excel\ExcelServiceProvider::class);
 //阿里云短信
 $app->register(\Lysice\Sms\SmsServiceProvider::class);
 //图片验证码
-// $app->register(Youngyezi\Captcha\CaptchaServiceProvider::class);
-// // 添加别名
-// $app->alias('captcha', 'Youngyezi\Captcha\CaptchaServiceProvider');
+$app->register(Youngyezi\Captcha\CaptchaServiceProvider::class);
+// 添加别名
+$app->alias('captcha', 'Youngyezi\Captcha\CaptchaServiceProvider');
 
 /*
 |--------------------------------------------------------------------------
