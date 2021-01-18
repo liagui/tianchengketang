3acd2489ab86585db76a40c2f627b1b2bcec48db
张泽坤 <31497246+zzk3279372968@users.noreply.github.com>
2021-01-18 17:36:08 +0800

    更新

--- app/Http/Controllers/Web/CourseController.php
+++ app/Http/Controllers/Web/CourseController.php
@@ -102,7 +102,7 @@ class CourseController extends Controller {
             }
 
         }
-        $subject = array_unique($subject, SORT_REGULAR);
+        $subject = array_values(array_unique($subject, SORT_REGULAR));
         return response()->json(['code' => 200 , 'msg' => '获取成功','data'=>$subject]);
     }
     /*
