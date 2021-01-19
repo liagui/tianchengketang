4813ccd1c944c160d542b43d525cd003a07d265b
liagui <315142971@qq.com>
2021-01-19 14:46:41 +0800

    登录

--- app/Http/Controllers/Admin/AuthenticateController.php
+++ app/Http/Controllers/Admin/AuthenticateController.php
@@ -57,40 +57,38 @@ class AuthenticateController extends Controller {
         try {
             $adminUserData = Admin::where(['username'=>$data['username']])->first();
             if (strlen($token = JWTAuth::attempt($data))<=0) {
-//                 //先查数据是否存在
-//                 if( !is_null($adminUserData) && !empty($adminUserData)){
-//                     if($adminUserData['login_err_number'] >= 5){
-//                          //判断时间是否过了60s
-//                          if(time()-$adminUserData['end_login_err_time']<=10){
-//                             return $this->response('你的密码已锁定，请5分钟后再试!', 401);
-//                          }else{
-//                              //走正常登录  并修改登录时间和登录次数
-//                              Admin::where("username",$data['username'])->update(['login_err_number'=>1,'end_login_err_time'=>time(),'updated_at'=>date('Y-m-d H:i:s')]);
-//                          }
-//                     }else{
-//                         //判断时间是否过了60s
-// //                        if(time()-$adminUserData['end_login_err_time']>=10){
-// //                            $userRes = Admin::where("username",$data['username'])->update(['login_err_number'=>1,'end_login_err_time'=>time(),'updated_at'=>date('Y-m-d H:i:s')]);
-// //                            if($userRes){
-// //                                DB::commit();
-// //                                return $this->response('密码错误，您还有4次机会!!!', 401);
-// //                            }
-// //                        }else{
-//                             $error_number = $adminUserData['login_err_number']+1;
-//                              //登录  并修改次数和登录时间
-//                             Admin::where("username",$data['username'])->update(['login_err_number'=>$error_number,'end_login_err_time'=>time(),'updated_at'=>date('Y-m-d H:i:s')]);
-//                             $err_number = 5-$error_number;
-//                             if($err_number <=0){
-//                                 return $this->response('你的密码已锁定，请5分钟后再试。', 401);
-//                             }
-//                             return $this->response('密码错误，您还有'.$err_number.'次机会！', 401);
-//                         }
+                //先查数据是否存在
+                if( !is_null($adminUserData) && !empty($adminUserData)){
+                    if($adminUserData['login_err_number'] >= 5){
+                         //判断时间是否过了60s
+                         if(time()-$adminUserData['end_login_err_time']<=10){
+                            return $this->response('你的密码已锁定，请5分钟后再试!', 401);
+                         }else{
+                             //走正常登录  并修改登录时间和登录次数
+                             Admin::where("username",$data['username'])->update(['login_err_number'=>1,'end_login_err_time'=>time(),'updated_at'=>date('Y-m-d H:i:s')]);
+                         }
+                    }else{
+                       //  //判断时间是否过了60s
+                       // if(time()-$adminUserData['end_login_err_time']>=10){
+                       //     $userRes = Admin::where("username",$data['username'])->update(['login_err_number'=>1,'end_login_err_time'=>time(),'updated_at'=>date('Y-m-d H:i:s')]);
+                       //      if($userRes){
 
-// //                    }
-
-//                 }else{
+                       //          return $this->response('密码错误，您还有4次机会!!!', 401);
+                       //      }
+                       //  }else{
+                            $error_number = $adminUserData['login_err_number']+1;
+                             //登录  并修改次数和登录时间
+                            Admin::where("username",$data['username'])->update(['login_err_number'=>$error_number,'end_login_err_time'=>time(),'updated_at'=>date('Y-m-d H:i:s')]);
+                            $err_number = 5-$error_number;
+                            if($err_number <=0){
+                                return $this->response('你的密码已锁定，请5分钟后再试。', 401);
+                            }
+                            return $this->response('密码错误，您还有'.$err_number.'次机会！', 401);
+                        // }
+                    }
+                }else{
                     return $this->response('账号密码错误', 401);
-//                 }
+                }
             }
         } catch (JWTException $e) {
             Log::error('创建token失败' . $e->getMessage());
@@ -98,23 +96,21 @@ class AuthenticateController extends Controller {
         }
         //验证严格的总控和中控
         $user = JWTAuth::user();
-        // if(!isset($user['school_status'])){
-        //     if($adminUserData['login_err_number']==5){
-        //         return $this->response('密码错误，您还有4次机会!', 401);
-        //     }else{
-        //         return $this->response('用户不合法', 401);
-        //     }
-        // }else{
-           if ($schoolStatus != $user->school_status) {
-               // if($adminUserData['login_err_number']==5){
-               //     return $this->response('密码错误，您还有4次机会!!', 401);
-               // }else{
+        if(!isset($user['school_status'])){
+            if($adminUserData['login_err_number']==5){
+                return $this->response('密码错误，您还有4次机会!', 401);
+            }else{
+                return $this->response('用户不合法', 401);
+            }
+        }else{
+            if ($schoolStatus != $user->school_status) {
+                if($adminUserData['login_err_number']==5){
+                   return $this->response('密码错误，您还有4次机会!!', 401);
+                }else{
                    return $this->response('用户不合法', 401);
-               // }
-           }
-        // }
-
-
+                }
+            }
+        }
         $schoolinfo = School::where('id',$user['school_id'])->select('name','end_time')->first();
         $user['school_name'] = $schoolinfo->name;
         $user['token'] = $token;
