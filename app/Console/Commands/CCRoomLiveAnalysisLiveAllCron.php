69802e8fb7c1cbbd3a1fd2e9e713ea9d80f1194e
马延平 <ms895@163.com>
2021-02-28 13:37:12 +0800

    更新一下 定时 任务的执行时间

--- app/Console/Commands/CCRoomLiveAnalysisLiveAllCron.php
+++ app/Console/Commands/CCRoomLiveAnalysisLiveAllCron.php
@@ -103,12 +103,12 @@ class CCRoomLiveAnalysisLiveAllCron extends Command
 
             foreach ($room_id_list as $room_id){
                 $CCRoomLiveAnalysisLiveCron->ProcessCCLiveUserWatch($cccloud,$room_id);
-                sleep(3);
+                sleep(1);
             }
 
 
             $current_time = strtotime("+1 day", $current_time);
-            $this->consoleAndLog('预期下一次执行时间:'.date('Y-m-d', strtotime($start_day)) . PHP_EOL);
+            $this->consoleAndLog('预期下一次执行时间:'.date('Y-m-d', strtotime($current_time)) . PHP_EOL);
 
             $this->consoleAndLog('统计时间：结束' . PHP_EOL);
         }
