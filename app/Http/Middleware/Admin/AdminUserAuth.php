ab7e191b39687b1185bade601ba0883b484302b9
tian cheng <yixuetiancheng@qq.com>
2021-04-28 14:21:25 +0800

    越权中间件

--- app/Http/Middleware/Admin/AdminUserAuth.php
+++ app/Http/Middleware/Admin/AdminUserAuth.php
@@ -18,7 +18,7 @@ class AdminUserAuth {
         switch($user['school_id']){
             case 1:
                 $admin = Admin::where(['school_id'=>$user['school_id'],'is_del'=>1,'is_forbid'=>1])->pluck('id')->toArray();
-                if(!empty($request->input('id')) && !in_array($user['school_id',$schoolIds)){
+                if(!empty($request->input('id')) && !in_array($user['school_id'],$schoolIds)){
                    return response()->json(['code' => 403 , 'msg' => '无权限！！！']);
                 }
                 // if(!empty($request->input('school_id')) && !in_array($request->input('school_id'),$schoolIds)){
