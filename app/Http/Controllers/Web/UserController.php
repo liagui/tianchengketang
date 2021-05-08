e2dfe6115f1c552397bc29f99f8f490a3bf9b189
tian cheng <yixuetiancheng@qq.com>
2021-05-08 15:49:52 +0800

    退出登录web

--- app/Http/Controllers/Web/UserController.php
+++ app/Http/Controllers/Web/UserController.php
@@ -637,11 +637,11 @@ class UserController extends Controller {
      */
     public function doLoginOut(){
         //获取用户id
-        $user_id =   $this->data['user_id'];
+        $user_id =   $this->data['user_info']['user_id'];
         //获取用户token
-        $token   =   $this->data['user_token'];
+        $token   =   $this->data['user_info']['user_token'];
         //获取手机号
-        $phone   =   $this->data['phone'];
+        $phone   =   $this->data['user_info']['phone'];
 
         //开启事务
         DB::beginTransaction();
